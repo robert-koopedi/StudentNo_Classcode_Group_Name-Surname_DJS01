8db0f66d84b9ce3e92968afe3fba82bb9347ed63
robert koopedi <koopedi05@gmail.com>
2025-03-16 01:15:10 +0200

    converted acceleration to seconds per min

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
+const convertAccToKmH2 = (acc) => acc * 12960;
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+function calcNewVel(vel, acc, time) {
+  const accKmH2 = acc * 12960; 
+  return vel + (acc*time /3600)
 }
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
